b08529cd001629d838e667d5b3c0467663d6cf43
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2024-07-14 21:40:52 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Untrusted Value
+This crate aim to provide a type-safe way to handle potentially untrusted values
+like user input.
+
+Problem statement: Example: a String coming from a user input should be considered untrusted
+while another String may be trusted application-wise. This difference
+should be enforced using the Rust type-system.
+It is therefore impossible to use user input or other untrusted values
+without sanitizing them first.
+
+## Example usage
+```rust
+# use UntrustedValues::{SanitizeValue, UntrustedValue};
+#
+struct UserDataType {data: i32}
+struct TrustedDataType {data: u32}
+
+# impl From<i32> for UserDataType {
+#     fn from(data: i32) -> Self {
+#         UserDataType {data}
+#     }
+# }
+# 
+# impl From<u32> for TrustedDataType {
+#     fn from(data: u32) -> Self {
+#         TrustedDataType {data}
+#     }
+# }
+# 
+impl SanitizeValue<TrustedDataType, ()> for UserDataType {
+    fn sanitize_value(self) -> Result<TrustedDataType, ()> {
+        Ok( (self.data.abs() as u32).into() )
+    }
+}
+
+let user_input: UserDataType = (-36).into();
+let user_input = UntrustedValue::from(user_input);
+
+let trusted_value: TrustedDataType = user_input.sanitize_value().expect("Sanitization failed");
+```
+
+If a type may be untrusted or not, the type `MaybeUntrusted` can be used.
+
+## Installation
+The tool is written in Rust, and can be installed using `cargo`:
+```bash
+cargo add untrusted-value
+```
+
+## Features
+The features enabled by default include:
+* `allow_usage_without_sanitization`: enables the method `use_untrusted_value`
+   to just use unpack an untrusted value. 
+
+## Contribution
+Contributions to the project are welcome! If you have a feature request,
+bug report, or want to contribute to the code, please open an
+issue or a pull request.
+
+## License
+This project is licensed under the MIT license. See the LICENSE file for details.
---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
+
+/// The type implementing this struct can be sanitized.
+///
+/// Calling `sanitize_value()` on the implementing type should return a sanitized version of the value.
+/// If the value cannot be sanitized, an error should be returned.
+pub trait SanitizeValue<Trusted, Error> {
+    /// Sanitizes the value.
+    /// The returned value might be of a different type.
+    ///
+    /// The returned value is sanitized and can be safely used.
+    /// If the value cannot be sanitized, an error must be returned.
+    fn sanitize_value(self) -> Result<Trusted, Error>;
+}
+
+/// Represents an untrusted/untrustworthy value.
+///
+/// An attacker might be able to control (part) of the returned value.
+/// Take special care processing this data.
+///
+/// See the method documentation of the function returning this value
+pub struct UntrustedValue<Insecure> {
+    value: Insecure,
+}
+
+impl<Insecure> UntrustedValue<Insecure> {
+    /// Be sure that you carefully handle the returned value since
+    /// it may be controllable by a malicious actor.
+    ///
+    /// See the method documentation of the function returning this value
+    #[cfg(feature = "allow_usage_without_sanitization")]
+    pub fn use_untrusted_value(self) -> Insecure {
+        self.value
+    }
+
+    /// Wraps the provided value as [UntrustedValue]
+    pub fn wrap(value: Insecure) -> Self {
+        UntrustedValue { value }
+    }
+}
+
+impl<Insecure> From<Insecure> for UntrustedValue<Insecure> {
+    /// Wraps the provided value as [UntrustedValue]
+    fn from(value: Insecure) -> Self {
+        UntrustedValue::wrap(value)
+    }
+}
+
+impl<Sanitized, Error, Insecure: SanitizeValue<Sanitized, Error>> SanitizeValue<Sanitized, Error> for UntrustedValue<Insecure> {
+    /// Sanitizes the value.
+    ///
+    /// The returned value is sanitized and can be safely used.
+    /// If the value cannot be sanitized, an error must be returned.
+    fn sanitize_value(self) -> Result<Sanitized, Error> {
+        self.value.sanitize_value()
+    }
+}
+
+/// Represents a value that might be untrusted. See UntrustedValue for more information.
+pub enum MaybeUntrusted<Insecure, Trusted> {
+    /// Trusted value variant
+    Ok(Trusted),
+    /// Untrusted value variant
+    Untrusted(UntrustedValue<Insecure>),
+}
+
+impl<Insecure> MaybeUntrusted<Insecure, Insecure> {
+    /// Be sure that you carefully handle the returned value since
+    /// it may be controllable by a malicious actor (when it is a MaybeUntrusted::Untrusted).
+    ///
+    /// See the method documentation of the function returning this value
+    #[cfg(feature = "allow_usage_without_sanitization")]
+    pub fn use_untrusted_value(self) -> Insecure {
+        match self {
+            MaybeUntrusted::Ok(value) => value,
+            MaybeUntrusted::Untrusted(value) => value.use_untrusted_value(),
+        }
+    }
+
+    /// Wraps the provided value as maybe untrusted, according to given boolean
+    pub fn wrap(value: Insecure, untrusted: bool) -> Self {
+        match untrusted {
+            true => Self::wrap_untrusted(value),
+            false => Self::wrap_ok(value),
+        }
+    }
+}
+
+impl<Insecure, Trusted> MaybeUntrusted<Insecure, Trusted> {
+    /// Returns true if the value is untrusted
+    pub fn is_untrusted(&self) -> bool {
+        match self {
+            MaybeUntrusted::Ok(_) => false,
+            MaybeUntrusted::Untrusted(_) => true,
+        }
+    }
+
+    /// Returns true if the value is not untrusted
+    pub fn is_ok(&self) -> bool {
+        !self.is_untrusted()
+    }
+
+    /// Wraps the provided values as Untrusted
+    pub fn wrap_untrusted(value: Insecure) -> Self {
+        MaybeUntrusted::Untrusted(value.into())
+    }
+
+    /// Wraps the provided values as Ok
+    pub fn wrap_ok(value: Trusted) -> Self {
+        MaybeUntrusted::Ok(value)
+    }
+}
+
+impl<Insecure, Trusted> From<UntrustedValue<Insecure>> for MaybeUntrusted<Insecure, Trusted> {
+    /// Converts an [UntrustedValue] to a [MaybeUntrusted] value
+    fn from(value: UntrustedValue<Insecure>) -> Self {
+        MaybeUntrusted::Untrusted(value)
+    }
+}
